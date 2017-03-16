a4e0bb87b2214d2b52986286c0f52d7e4f5ded8a
Igor Ranieri Elland <igor@elland.me>
2017-03-16 11:13:28 +0100

    Add missing headers.

--- Example/AttachmentPickerController/AttachmentPickerController.h
+++ Example/AttachmentPickerController/AttachmentPickerController.h
@@ -1,19 +1,6 @@
-//
-//  AttachmentPickerController.h
-//  AttachmentPickerController
-//
-//  Created by Igor Ranieri on 16.03.17.
-//  Copyright © 2017 Denis Bogatyrev. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
-//! Project version number for AttachmentPickerController.
 FOUNDATION_EXPORT double AttachmentPickerControllerVersionNumber;
-
-//! Project version string for AttachmentPickerController.
 FOUNDATION_EXPORT const unsigned char AttachmentPickerControllerVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <AttachmentPickerController/PublicHeader.h>
-
-
+#import <AttachmentPickerController/DBAttachmentPickerController.h>
